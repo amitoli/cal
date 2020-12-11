4c738297b0a20b773e66f173cc4c4ad02266a008
Pradeep Kumar Sahoo <sahoo.pradeep.nits@gmail.com>
2020-12-11 22:48:29 +0530

    added deleteEvent API

--- src/main/java/com/expedia/calender/controller/EventController.java
+++ src/main/java/com/expedia/calender/controller/EventController.java
@@ -5,9 +5,11 @@ import com.expedia.calender.service.IEventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,6 +27,12 @@ public class EventController
 	public ResponseEntity<Event> createEvent(@RequestBody Event event){
 		Event eventCreated = eventService.createEvent(event);
 		return ResponseEntity.status(HttpStatus.CREATED).body(eventCreated);
+	}
+
+	@DeleteMapping
+	public ResponseEntity<Void> deleteEvent(@RequestParam int eventId){
+		//eventService.deleteEvent(eventId);
+		return ResponseEntity.status(HttpStatus.OK).build();
 	}
 
 }
