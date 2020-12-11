7e42c9ae64943f999d723186e84bfad3db2bc4ad
Pradeep Kumar Sahoo <sahoo.pradeep.nits@gmail.com>
2020-12-11 21:47:58 +0530

    lombok added

--- src/main/java/model/Event.java
+++ src/main/java/model/Event.java
@@ -6,8 +6,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Event
 {
 	@Id
@@ -18,64 +22,4 @@ public class Event
 	LocalDateTime endTime;
 	String location;
 	String owner;
-
-	public int getId()
-	{
-		return id;
-	}
-
-	public void setId(int id)
-	{
-		this.id = id;
-	}
-
-	public String getTitle()
-	{
-		return title;
-	}
-
-	public void setTitle(String title)
-	{
-		this.title = title;
-	}
-
-	public LocalDateTime getStartTime()
-	{
-		return startTime;
-	}
-
-	public void setStartTime(LocalDateTime startTime)
-	{
-		this.startTime = startTime;
-	}
-
-	public LocalDateTime getEndTime()
-	{
-		return endTime;
-	}
-
-	public void setEndTime(LocalDateTime endTime)
-	{
-		this.endTime = endTime;
-	}
-
-	public String getLocation()
-	{
-		return location;
-	}
-
-	public void setLocation(String location)
-	{
-		this.location = location;
-	}
-
-	public String getOwner()
-	{
-		return owner;
-	}
-
-	public void setOwner(String owner)
-	{
-		this.owner = owner;
-	}
 }
